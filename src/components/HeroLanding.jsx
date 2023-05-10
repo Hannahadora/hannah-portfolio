75faaccaa1cb92880b4751e70f9729e83727599a
hannah <hannahadora97@gmail.com>
2023-05-10 20:26:52 +0100

    update

--- src/components/HeroLanding.jsx
+++ src/components/HeroLanding.jsx
@@ -26,7 +26,7 @@ const HeroLanding = () => {
             <section className='page-wrapper flex md:flex-row flex-col-reverse items-center justify-between md:pb-0 pb-7'>
                 <div ref={introRef} className='md:text-left text-center md:mt-[134px] mt-[40px] relative z-10'>
                     <h1 className='lg:text-[70px] md:text-[50px] text-[30px] leading-none'>Nice to <br className='xl:hidden md:block hidden' /> meet you! <br className='xl:block md:hidden block' /> I'm  <span className='border-b border-green-500'><br className='xl:hidden md:block hidden' />Hannah Ejimofor</span>.</h1>
-                    <p className='md:max-w-[380px] w-[380px] md:text-[18px] text-[16px] md:mx-0 mx-auto py-[50px]'>Based in the Nigeria, I'm a front-end developer passionate about building accessible web apps that users love.</p>
+                    <p className='md:max-w-[380px] w-[380px] md:text-[18px] text-[16px] md:mx-0 mx-auto py-[50px] lg:px-0 px-[16px]'>Based in the Nigeria, I'm a front-end developer passionate about building accessible web apps that users love.</p>
                     <a href='/files/hannah-resume.pdf' download={"Hannah's Resume"} target={'_blank'}><CustomButton text={'Download Resume'} /></a>
                 </div>
                 <div ref={imageRef} className='md:max-w-[500px] max-w-[350px] md:absolute -top-20 lg:right-0 -right-24'>
