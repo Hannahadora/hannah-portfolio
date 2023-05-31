983fa0d478dcc16cdb5c7500677c1211b855a489
hannah <hannahadora97@gmail.com>
2023-05-31 18:24:43 +0100

    update

--- src/utils/data.js
+++ src/utils/data.js
@@ -1,21 +1,37 @@
-import sampel_img_1 from '../assests/images/projects/pay-api-page.png'
-import sampel_img_2 from '../assests/images/projects/e-learning-page.png'
-import sampel_img_3 from '../assests/images/projects/todo-app-page.png'
-import sampel_img_4 from '../assests/images/projects/url-shortner-page.png'
-import sampel_img_5 from '../assests/images/projects/product-feedback-page.png'
-import sampel_img_6 from '../assests/images/projects/chuck-noris-jokes-2.png'
-import sampel_img_7 from '../assests/images/projects/corniehealth-page.png'
-import sampel_img_8 from '../assests/images/projects/ighopay-page.png'
-import sampel_img_9 from '../assests/images/projects/ayagigs-page.png'
-import sampel_img_10 from '../assests/images/projects/paxity-page.png'
-import sampel_img_11 from '../assests/images/projects/dflab-page.png'
-import sampel_img_12 from '../assests/images/projects/corniehealth-app-page.png'
+import sample_img_1 from '../assests/images/projects/pay-api-page.png'
+import sample_img_2 from '../assests/images/projects/e-learning-page.png'
+import sample_img_3 from '../assests/images/projects/todo-app-page.png'
+import sample_img_4 from '../assests/images/projects/url-shortner-page.png'
+import sample_img_5 from '../assests/images/projects/product-feedback-page.png'
+import sample_img_6 from '../assests/images/projects/chuck-noris-jokes-2.png'
+import sample_img_7 from '../assests/images/projects/corniehealth-page.png'
+import sample_img_8 from '../assests/images/projects/ighopay-page.png'
+import sample_img_9 from '../assests/images/projects/ayagigs-page.png'
+import sample_img_10 from '../assests/images/projects/paxity-page.png'
+import sample_img_11 from '../assests/images/projects/dflab-page.png'
+import sample_img_12 from '../assests/images/projects/corniehealth-app-page.png'
+import sample_img_13 from '../assests/images/projects/invoice-app-page.png'
+import sample_img_14 from '../assests/images/projects/tita-web3-page.png'
 
 const projects = [
     // React
     {
+        title:'TITA Celo Web3 Project',
+        image:sample_img_14,
+        stacks:['html', 'css', 'react', 'web3', "celo"],
+        type: 'react',
+        url: "https://tita-web3.vercel.app/"
+    },
+    {
+        title:'Invoice App',
+        image:sample_img_13,
+        stacks:['html', 'css', 'react', 'formik', "gsap", "yup", "redux"],
+        type: 'react',
+        url: "https://invoice-users-app.vercel.app/"
+    },
+    {
         title:'Pay-API',
-        image:sampel_img_1,
+        image:sample_img_1,
         stacks:['html', 'css', 'react', 'formik'],
         type: 'react',
         url: "https://pay-api-react-app.vercel.app/"
@@ -25,77 +41,77 @@ const projects = [
     // Vue
     {
         title:'Ighopay',
-        image:sampel_img_8,
+        image:sample_img_8,
         stacks:['html', 'css', 'Nuxt', 'Javascript', 'Vuex'],
         type: 'vue',
         url: "http://hospitals.eirs.gov.ng/"
     },
     {
         title:'CornieHealth',
-        image:sampel_img_7,
+        image:sample_img_7,
         stacks:['html', 'css', 'Nuxt', 'TypeScript', 'JavaScript', 'Vuex'],
         type: 'vue',
         url: "https://corniehealth.com/"
     },
     {
         title:'CornieHealth App',
-        image:sampel_img_12,
+        image:sample_img_12,
         stacks:['html', 'css', 'vue 3', 'TypeScript', 'Vuex'],
         type: 'vue',
         url: "https://app.corniehealth.com/signin"
     },
     {
         title:'Ayagigs',
-        image:sampel_img_9,
+        image:sample_img_9,
         stacks:['html', 'css', 'Nuxt 3', 'pinia', 'TypeScript'],
         type: 'vue',
         url: "https://ayagigs-web.staging.chigisoft.co/"
     },
     {
         title:'Paxity',
-        image:sampel_img_10,
+        image:sample_img_10,
         stacks:['html', 'css', 'Nuxt 3', 'pinia', 'TypeScript'],
         type: 'vue',
         url: "https://paxity-web.projects.chigisoft.co/"
     },
     {
         title:'Digital Forensic Lab',
-        image:sampel_img_11,
+        image:sample_img_11,
         stacks:['html', 'css', 'Vue', 'vuex', 'Javascript'],
         type: 'vue',
         url: "https://df-lab-web.projects.chigisoft.co/"
     },
     {
         title:'Chuck Norris Jokes',
-        image:sampel_img_6,
+        image:sample_img_6,
         stacks:['html', 'css', 'javascript', "vue 3", "vite", "pinia"],
         url: "https://hannah-anamen-technical-assessment.netlify.app/",
         type: "vue"
     },
     {
         title:'E-Learning Landing Page',
-        image:sampel_img_2,
+        image:sample_img_2,
         stacks:['html', 'css', 'vue 3', 'vite', 'pinia'],
         type: 'vue',
         url: "https://e-skills.netlify.app/"
     },
     {
         title:'Product Feedback',
-        image:sampel_img_5,
+        image:sample_img_5,
         stacks:['html', 'css', 'javascript', 'vue', 'vuex' ],
         url: "https://product-feedbacks.netlify.app/",
         type: 'vue'
     },
     {
         title:'Todo Web App',
-        image:sampel_img_3,
+        image:sample_img_3,
         stacks:['html', 'css', 'vuejs', 'vuex' ],
         type: 'vue',
         url: "https://anntodoapp.netlify.app/"
     },
     {
         title:'URL Shortener',
-        image:sampel_img_4,
+        image:sample_img_4,
         stacks:['html', 'css', 'vue', 'javascript' ],
         type: 'vue',
         url: "https://shortenlink.netlify.app/"
