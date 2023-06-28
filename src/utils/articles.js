8aea972e2eb82567a93f60358937b172eac96d61
hannah <hannahadora97@gmail.com>
2023-06-28 13:45:59 +0100

    update on skills

--- src/utils/articles.js
+++ src/utils/articles.js
@@ -1,5 +1,6 @@
 import blog_img_1 from '../assests/images/blog/nuxt_pinia.png'
 import blog_img_2 from '../assests/images/blog/aws_network_topology.png'
+import blog_img_3 from '../assests/images/blog/aws_ec2_alb_asg.png'
 
 const articles = [
     // Frontend
@@ -18,6 +19,13 @@ const articles = [
         desc:"This article explores how to build a scalable network infrastructure on AWS using the AWS Management Console. Specifically, we will focus on setting up a Virtual Private Cloud (VPC) with private and public subnets, an Internet Gateway (IGW), route tables, and Network Address Translation (NAT) to establish secure and reliable connectivity",
         type: 'devops',
         url: "https://medium.com/@hannahadora97/building-a-scalable-network-infrastructure-on-aws-bcd2e7d4f39c"
+    },
+    {
+        title:'Deploying a Highly Available Web Server on AWS With ASG and ALB: Step-by-Step Guide Using the AWS Management Console',
+        image:blog_img_3,
+        desc:"This article explores how to achieve High Availability and Fault Tolerant with Auto Scaling Groups and Application Load Balancers",
+        type: 'devops',
+        url: "https://medium.com/@hannahadora97/deploying-a-highly-available-web-server-on-aws-with-asg-and-alb-step-by-step-guide-using-the-aws-654e07acb530"
     },
    
 ]
